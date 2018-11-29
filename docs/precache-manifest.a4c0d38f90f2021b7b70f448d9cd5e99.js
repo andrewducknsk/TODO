ab3767e55a3797bf2544662b4f598c4ce9523eb5
andrewducknsk <32197680+andrewducknsk@users.noreply.github.com>
2018-11-29 14:09:41 +0700

    update docs

---
+++ docs/precache-manifest.a4c0d38f90f2021b7b70f448d9cd5e99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b6d35d00a980b48277c7",
+    "url": "/static/js/main.b6d35d00.chunk.js"
+  },
+  {
+    "revision": "f075e82a6e53c58e544a",
+    "url": "/static/js/1.f075e82a.chunk.js"
+  },
+  {
+    "revision": "b6d35d00a980b48277c7",
+    "url": "/static/css/main.202024cd.chunk.css"
+  },
+  {
+    "revision": "0f6eb2a17e72f73621fbecfb8c75cd89",
+    "url": "/index.html"
+  }
+];
